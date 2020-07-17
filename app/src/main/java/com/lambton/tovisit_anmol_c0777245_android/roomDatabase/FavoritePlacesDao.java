d48d2dcd04c2305bc2d3fba01f7eeac6ec10665d
Anmol Singh <anmolsingh1597@gmail.com>
2020-07-17 14:27:13 -0400

    [Anmol Singh]Room database: Favorite Places Dao defined

--- app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/roomDatabase/FavoritePlacesDao.java
+++ app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/roomDatabase/FavoritePlacesDao.java
@@ -1,7 +1,28 @@
 package com.lambton.tovisit_anmol_c0777245_android.roomDatabase;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
 
 @Dao
 public interface FavoritePlacesDao {
+
+    @Insert
+    void insertFavoritePlaces(FavoritePlaces favoritePlaces);
+
+    @Query("DELETE FROM favoriteplaces")
+    void deleteAllFavoritePlaces();
+
+    @Query("DELETE FROM favoriteplaces WHERE id = :id")
+    int favoritePlaces(int id);
+
+    @Query("UPDATE favoriteplaces SET latLng = :latLng, assignedDate= :assignedDate, locationName = :locationName WHERE id = :id")
+    int updateFavoritePlaces(int id, LatLng latLng, String assignedDate, String locationName);
+
+    @Query("SELECT * FROM favoriteplaces ORDER BY locationName")
+    List<FavoritePlaces> getAllFavoritePlaces();
 }
