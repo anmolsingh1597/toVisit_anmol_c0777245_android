2facc9b7b800653eee5e3cc42fa471e631f20bcc
Anmol Singh <anmolsingh1597@gmail.com>
2020-07-16 23:13:41 -0400

    [Anmol Singh]Maps Fragment: Distinguished Destination Marker assigned over map

--- app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/fragment/MapsFragment.java
+++ app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/fragment/MapsFragment.java
@@ -2,22 +2,47 @@ package com.lambton.tovisit_anmol_c0777245_android.fragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.lambton.tovisit_anmol_c0777245_android.R;
 
 public class MapsFragment extends Fragment {
+
+    private static final String TAG = "MapsFragment";
+
+    FusedLocationProviderClient mClient;
+
+    GoogleMap mMap;
+
+    LatLng userLocation;
+
+    public GoogleMap getmMap() {
+        return mMap;
+    }
+
+    public LatLng getDestLocation() {
+        return destLocation;
+    }
+
+    LatLng destLocation;
+    Location destination;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -32,11 +57,54 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap = googleMap;
+            mMap.setOnMarkerDragListener(MapsFragment.this);
+            mMap.setMyLocationEnabled(true);
+
+            mClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                        mMap.addMarker(new MarkerOptions().position(latLng));
+                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+                        ((MainActivity)getActivity()).showLaunchNearbyPlaces(latLng);
+                    }
+                }
+            });
+
+            // add long press gesture on map
+            googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                @Override
+                public void onMapLongClick(LatLng latLng) {
+                    destLocation = latLng;
+                    destination = new Location("Your destination");
+                    destination.setLatitude(latLng.latitude);
+                    destination.setLongitude(latLng.longitude);
+//                    setMarker(destLocation);
+                    setMarker(destination);
+                }
+            });
         }
     };
+
+
+    private void setMarker(Location destination) {
+        LatLng destLatLng = new LatLng(destination.getLatitude(), destination.getLongitude());
+        setMarker(destLatLng);
+    }
+
+    private void setMarker(LatLng location) {
+        MarkerOptions options = new MarkerOptions().position(location)
+                .title("Your Destination")
+                .snippet("You are going there")
+                .draggable(true);
+        mMap.addMarker(options);
+    }
 
     @Nullable
     @Override
