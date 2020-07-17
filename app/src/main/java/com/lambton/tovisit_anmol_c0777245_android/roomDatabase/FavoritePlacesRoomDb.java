78f5c6db5bd88320b9b865c94fce9622b4a4d13f
Anmol Singh <anmolsingh1597@gmail.com>
2020-07-17 14:29:37 -0400

    [Anmol Singh]Room database: Database defined

--- app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/roomDatabase/FavoritePlacesRoomDb.java
+++ app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/roomDatabase/FavoritePlacesRoomDb.java
@@ -1,4 +1,28 @@
 package com.lambton.tovisit_anmol_c0777245_android.roomDatabase;
 
-public class FavoritePlacesRoomDb {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {FavoritePlaces.class}, version = 1, exportSchema = false)
+public abstract class FavoritePlacesRoomDb extends RoomDatabase {
+
+    private static final String DB_NAME = "favorite_places_database";
+
+    public abstract FavoritePlacesDao favoritePlacesDao();
+
+    private static volatile FavoritePlacesRoomDb INSTANCE;
+
+    public static FavoritePlacesRoomDb getINSTANCE(Context context){
+        if(INSTANCE == null){
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), FavoritePlacesRoomDb.class, DB_NAME)
+                    .allowMainThreadQueries()
+                    .build();
+        }
+
+        return INSTANCE;
+
+    }
 }
