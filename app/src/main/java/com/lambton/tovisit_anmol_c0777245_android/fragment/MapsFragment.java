2aa9f72420cd78df66bd6b861a083d9c5976ae5d
Anmol Singh <anmolsingh1597@gmail.com>
2020-07-17 00:42:35 -0400

    [Anmol Singh]Application: UI redesigned

--- app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/fragment/MapsFragment.java
+++ app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/fragment/MapsFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
