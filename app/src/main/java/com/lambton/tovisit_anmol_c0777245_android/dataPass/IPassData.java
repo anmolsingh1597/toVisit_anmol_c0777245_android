3004970fcd674e9a82cb814216f58d37ecc53995
Anmol Singh <anmolsingh1597@gmail.com>
2020-07-16 23:16:17 -0400

    [Anmol Singh]IPassData: Designed

---
+++ app/src/main/java/com/lambton/tovisit_anmol_c0777245_android/dataPass/IPassData.java
@@ -0,0 +1,9 @@
+package com.lambton.tovisit_anmol_c0777245_android.dataPass;
+
+import android.location.Location;
+
+import com.google.android.gms.maps.GoogleMap;
+
+public interface IPassData {
+    void destinationSelected(Location location, GoogleMap map);
+}
